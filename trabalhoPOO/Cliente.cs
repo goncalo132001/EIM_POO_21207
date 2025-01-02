bdc7faeaaa554887a98e6d9da921e542f89402ab
goncalo132001 <a21207@alunos.ipca.pt>
2025-01-02 19:13:04 +0000

    Fase 1 - Atualização

--- trabalhoPOO/Cliente.cs
+++ trabalhoPOO/Cliente.cs
@@ -1,36 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace trabalhoPOO
 {
-    public class Cliente
+    public class Cliente:Pessoa
     {
         #region Atributos
-        private string nome;
-        private string morada;
+        public int nConta;
+
         #endregion
 
         #region Metodos
 
         #region Construtores
 
-        public Cliente(string nome)
+        public Cliente(string nome,int nConta,int idade):base(nome)
         {
+            this.nConta = nConta;
             this.nome = nome;
+            this.idade = idade;
         }
+        public Cliente()
+        {
+            
+
+        }
+
 
         #endregion
 
         #region Propriedades
-
-        public string Nome 
-        { 
-            get { return nome; }
-            set { nome = value; }
+        public int NConta
+        {
+            get { return nConta; }
+            set { nConta = value; }
         }
+
         #endregion
 
 
---
+++ trabalhoPOO/Pessoa.cs
@@ -0,0 +1,45 @@
+﻿namespace trabalhoPOO
+{
+    public class Pessoa
+    {
+        #region Atributos
+        public string nome;
+        public int idade;
+
+        public Pessoa(string nome)
+        {
+            this.nome = nome;
+        }
+        #endregion
+
+        #region Metodos
+
+        #region Construtores
+
+        public Pessoa(int idade=10,string nome="por fazer")
+        {
+            this.nome = nome;
+            this.idade = idade;
+        }
+
+        #endregion
+
+        #region Propriedades
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        #endregion
+
+        public int Idade
+        {
+            get { return idade; }   
+            set => idade = value;
+        }
+        #endregion
+
+
+    }
+}
